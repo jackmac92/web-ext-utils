1a0055ffd08c8eb7fb4419e005cfc61037bca877
Jack McCown <jmccown@cbinsights.com>
2019-09-30 11:38:42 -0400

    add extra method

--- src/index.ts
+++ src/index.ts
@@ -63,3 +63,18 @@ comPort.onMessage.addListener(m => {
   console.log(`Inject received msg ${m}`)
   // get branchInfo, open locally, allow for changes to be saved to git patch and sent over to author
 })
+
+const awaitTabClosing = async (targetTabId, msTimeout = 60000) =>
+  new Promise((resolve, reject) => {
+    const timeout = setTimeout(() => reject('timeout'), msTimeout)
+    const tmpHandler = (tabId, _info) => {
+      if (tabId === targetTabId) {
+        clearTimeout(timeout)
+        browser.tabs.onRemoved.removeListener(tmpHandler)
+        resolve()
+      }
+    }
+    browser.tabs.onRemoved.addListener(tmpHandler)
+  })
+
+export default awaitTabClosing
