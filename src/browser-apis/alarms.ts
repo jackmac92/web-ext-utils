183720ad8f45da7448c2c88f75ae8d22ee88c941
Jack McCown <jmccown@cbinsights.com>
2020-01-09 21:22:55 -0500

    feat: add support for some web-ext alarms

---
+++ src/browser-apis/alarms.ts
@@ -0,0 +1,41 @@
+import { browser } from 'webextension-polyfill-ts'
+
+type intervalType = (a: () => void, b: number) => () => void
+
+export const browserSetTimeout: intervalType = (cb, ms) => {
+  const cbUuid = `${Math.random()}`
+  browser.alarms.create(cbUuid, {
+    when: Date.now() + ms
+  })
+  const cleanup = () => {
+    browser.alarms.onAlarm.removeListener(alarmHandler)
+    browser.alarms.clear(cbUuid)
+  }
+  const alarmHandler = ({ name }) => {
+    if (name === cbUuid) {
+      cleanup()
+      cb()
+    }
+  }
+  browser.alarms.onAlarm.addListener(alarmHandler)
+  return cleanup
+}
+
+export const browserSetInterval: intervalType = (cb, ms) => {
+  const cbUuid = `${Math.random()}`
+  browser.alarms.create(cbUuid, {
+    periodInMinutes: ms / 1000 / 60,
+    when: Date.now() + ms
+  })
+  const cleanup = () => {
+    browser.alarms.onAlarm.removeListener(alarmHandler)
+    browser.alarms.clear(cbUuid)
+  }
+  const alarmHandler = ({ name }) => {
+    if (name === cbUuid) {
+      cb()
+    }
+  }
+  browser.alarms.onAlarm.addListener(alarmHandler)
+  return cleanup
+}
