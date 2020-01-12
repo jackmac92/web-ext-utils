6787f2bb07515361812b4fdf6ee9ed93c392b22e
Jack McCown <jmccown@cbinsights.com>
2020-01-12 15:42:06 -0500

    chore: figure out how to get the tests working

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,12 @@ module.exports = {
   testRegex: '(^src.*/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
   testPathIgnorePatterns: ['/node_modules/', '/lib/'],
   setupFiles: ['jest-webextension-mock'],
+  globals: {
+    // window needs to be set as global so that webextension-polyfill-ts exports the full object
+    // https://github.com/Lusito/webextension-polyfill-ts/blob/develop/src/generated/index.ts#L169
+    // HACK this will likely break something at some point in the future
+    window: true
+  },
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
   }
--- src/browser-apis/__tests__/storage.spec.ts
+++ src/browser-apis/__tests__/storage.spec.ts
@@ -1,8 +1,13 @@
 import { getLocalStorage } from '../storage'
+import { mocked } from 'ts-jest/utils'
+import { browser } from 'webextension-polyfill-ts'
+jest.mock('webextension-polyfill-ts')
 
-describe('your function to test', () => {
-  it('should have called a webextension API', () => {
-    getLocalStorage('testkey')
-    // expect(browser.storage.local.get).toHaveBeenCalledWith(['testkey'])
+const mockBrowser = mocked(browser, true)
+
+describe('web-ext storage', () => {
+  it('helper method for local storage access should call the underlying API', () => {
+    expect(() => getLocalStorage('testkey')).not.toThrowError()
+    expect(mockBrowser.storage.local.get).toHaveBeenCalledWith(['testkey'])
   })
 })
