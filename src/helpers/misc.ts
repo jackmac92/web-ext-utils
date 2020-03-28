6efe3ea31ebd6cdff438284cb1bc8c8745c353e3
Jack McCown <jmccown@cbinsights.com>
2020-03-28 19:27:38 -0400

    fix: make return type of getInstallid non nullable
    assume ensureInstallId is called

--- src/helpers/misc.ts
+++ src/helpers/misc.ts
@@ -1,23 +1,25 @@
-import { getLocalStorage, setLocalStorage } from '../browser-apis/storage'
+import { browser } from "webextension-polyfill-ts";
+import { getLocalStorage, setLocalStorage } from "../browser-apis/storage";
 
-const installIdKey = 'applicationId__unieq'
+const installIdKey = "applicationId__unieq";
 
-export const getInstallId = () =>
-  getLocalStorage<string, string | null>(installIdKey)
+export const getInstallId = () => getLocalStorage<string, string>(installIdKey);
 
 export const ensureInstallId = async () => {
-  const localId = await getInstallId()
+  const localId = await getInstallId();
   if (!localId || localId.length === 0) {
-    const result = Math.random() * 1000000000000
-    await setLocalStorage(installIdKey, result)
+    const result = Math.random() * 1000000000000;
+    await setLocalStorage(installIdKey, result);
   }
-}
+};
+
+export const getApplicationId = () => browser.runtime.id;
 
 export const oneShotEventHandler = eventType =>
   new Promise(resolve => {
     const handlerHelper = (...eventArgs) => {
-      resolve(...eventArgs)
-      eventType.removeListener(handlerHelper)
-    }
-    eventType.addListener(handlerHelper)
-  })
+      resolve(...eventArgs);
+      eventType.removeListener(handlerHelper);
+    };
+    eventType.addListener(handlerHelper);
+  });
