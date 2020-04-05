5018b351b37635bd10d8a875a7cbcc941ef8d07a
Jack McCown <jmccown@cbinsights.com>
2020-04-05 18:19:31 -0400

    feat: add support for interactive banner insertion

--- src/helpers/displayBanner.ts
+++ src/helpers/displayBanner.ts
@@ -1,9 +1,18 @@
-import { browser, Tabs } from 'webextension-polyfill-ts' // eslint-disable-line no-unused-vars
-import { getActiveTab } from '../browser-apis/tabs'
+import { browser, Tabs } from "webextension-polyfill-ts"; // eslint-disable-line no-unused-vars
+import { nanoid } from "nanoid";
+import { getActiveTab } from "../browser-apis/tabs";
+import { oneShotEventHandler } from "./misc";
 
 const addSelfCleaningBannerToTab = (message: string) => async (
   tabId: number
 ) => {
+  const bannerUuid = nanoid();
+  const waitForResponse = oneShotEventHandler(
+    browser.runtime.onMessage,
+    async (message, _sender, _sendResponse) =>
+      message.type === "USER_CONFIRM" && message.uuid === bannerUuid
+  );
+  // SHOULDDO move the below code to a separate file to be loaded in, probably need to figure out web_accessible_resources, in order to not use chrome.runtime (use browser.runtime)
   await browser.tabs.executeScript(tabId, {
     code: `\
     const banner = document.createElement('div')
@@ -15,8 +24,20 @@ const addSelfCleaningBannerToTab = (message: string) => async (
     banner.style.left = 0
     banner.style.background = 'green'
     banner.style.color = 'yellow'
+    banner.style.display = 'flex'
+    banner.style['align-items'] = 'center'
+    banner.style['justify-content'] = 'center'
 
-    banner.innerText = '${message}'
+    const copyContainer = document.createElement('span')
+    copyContainer.innerText = '${message}'
+    const confirmButton = document.createElement('button')
+    confirmButton.innerText = 'Yes!'
+    confirmButton.onclick = () => chrome.runtime.sendMessage({ type: 'USER_CONFIRM', uuid: "${bannerUuid}" })
+    const rejectButton = document.createElement('button')
+    rejectButton.innerText = 'Nope'
+    banner.appendChild(copyContainer)
+    banner.appendChild(confirmButton)
+    banner.appendChild(rejectButton)
     document.body.appendChild(banner)
     document.addEventListener('visibilitychange', (_e) => {
         if (document.hidden) {
@@ -24,11 +45,12 @@ const addSelfCleaningBannerToTab = (message: string) => async (
         }
     });
   `
-  })
-}
+  });
+  return waitForResponse;
+};
 
-export const addBannerToActiveTab = (activityName = 'Unknown Task') => {
-  const addBannerToTab = addSelfCleaningBannerToTab(activityName)
-  getActiveTab().then((tab: Tabs.Tab) => addBannerToTab(tab.id))
-  browser.tabs.onActivated.addListener(({ tabId }) => addBannerToTab(tabId))
-}
+export const addBannerToActiveTab = (activityName = "Unknown Task") => {
+  const addBannerToTab = addSelfCleaningBannerToTab(activityName);
+  getActiveTab().then((tab: Tabs.Tab) => addBannerToTab(tab.id));
+  browser.tabs.onActivated.addListener(({ tabId }) => addBannerToTab(tabId));
+};
