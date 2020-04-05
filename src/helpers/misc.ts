0464bab3111e6cb1018fc855e74ba35a77400275
Jack McCown <jmccown@cbinsights.com>
2020-04-05 18:18:25 -0400

    feat: add support for matching specific event in oneshot

--- src/helpers/misc.ts
+++ src/helpers/misc.ts
@@ -27,11 +27,29 @@ export const ensureInstallId = async (): Promise<string> => {
 
 export const getApplicationId = () => browser.runtime.id;
 
-export const oneShotEventHandler = eventType =>
-  new Promise(resolve => {
-    const handlerHelper = (...eventArgs) => {
-      resolve(...eventArgs);
-      eventType.removeListener(handlerHelper);
+const defaultEventMatcher = () => Promise.resolve(true);
+
+export const oneShotEventHandler = (
+  eventType,
+  matchesTargetEvent: (...args: any[]) => Promise<boolean> = defaultEventMatcher
+) =>
+  new Promise((resolve, reject) => {
+    const handlerTimeout = setTimeout(reject, 1000 * 60);
+    const handlerHelper = (...eventArgs: any[]) => {
+      Promise.resolve(matchesTargetEvent(...eventArgs))
+        .then(isTheCorrectEvent => {
+          if (isTheCorrectEvent) {
+            resolve(...eventArgs);
+            clearTimeout(handlerTimeout);
+            eventType.removeListener(handlerHelper);
+          }
+        })
+        .catch(err => {
+          console.warn(
+            "Ignoring error encountered when checking for target event",
+            err
+          );
+        });
     };
     eventType.addListener(handlerHelper);
   });
