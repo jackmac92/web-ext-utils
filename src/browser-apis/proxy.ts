777497363ffc07f379038e3fe98765614544b3a8
Jack McCown <jmccown@cbinsights.com>
2020-04-05 22:10:57 -0400

    feat: WIP setup for managing proxies

---
+++ src/browser-apis/proxy.ts
@@ -0,0 +1,16 @@
+import { browser } from "webextension-polyfill-ts";
+
+export const stolenDemoCode = () => {
+  const config = {
+    mode: "fixed_servers",
+    rules: {
+      proxyForHttp: {
+        scheme: "socks5",
+        host: "1.2.3.4"
+      },
+      bypassList: ["foobar.com"]
+    }
+  };
+
+  browser.proxy.settings.set({ value: config, scope: "regular" });
+};
