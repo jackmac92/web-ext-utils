a783fe60eb97387cd1829efd52f4729c8280b413
Jack McCown <jmccown@cbinsights.com>
2020-04-05 22:30:47 -0400

    fix: move scrapePage to correct place

---
+++ src/contentScripts/scrapePage.ts
@@ -0,0 +1,48 @@
+import { browser } from "webextension-polyfill-ts";
+
+type validationHandler = (el: any) => boolean;
+type locateElementHandler = () => any;
+
+const MAX_ATTEMPTS = 20;
+
+export const elementLocated = (
+  el: any,
+  validator: validationHandler = () => true
+) => {
+  if (el === undefined) {
+    return false;
+  }
+
+  if (validator !== undefined) {
+    return validator(el);
+  }
+
+  return true;
+};
+
+export const scrapeInfo = async (
+  item: string,
+  locator: locateElementHandler,
+  validator: validationHandler
+) => {
+  const comPort = browser.runtime.connect();
+  const payload = await new Promise((resolve, reject) => {
+    let element: any;
+    let attempts = 0;
+    const waitForEl = setInterval(() => {
+      if (elementLocated(element, validator)) {
+        clearInterval(waitForEl);
+        resolve({
+          action: item,
+          data: element
+        });
+      } else if (attempts < MAX_ATTEMPTS) {
+        attempts += 1;
+        element = locator();
+      } else {
+        reject("Failing to locate element");
+      }
+    }, 1000);
+  });
+  return comPort.postMessage({ type: "SCRAPE_INFO", payload });
+};
