15ec99b677a63ca494888ff9fd918be822dfe403
Jack McCown <jmccown@cbinsights.com>
2020-04-07 23:56:32 -0400

    fix: update displayBanner

--- src/helpers/displayBanner.ts
+++ src/helpers/displayBanner.ts
@@ -17,41 +17,40 @@ const addSelfCleaningBannerToTab = (message: string) => async (
   await browser.tabs
     .executeScript(tabId, {
       code: `\
-    console.log('Begin inserting banner')
-    const banner = document.createElement('div')
-    banner.id = 'browixir-banner'
-    banner.style.width = '100vw'
-    banner.style.height = '8vw'
-    banner.style.position = 'absolute'
-    banner.style.top = 0
-    banner.style.left = 0
-    banner.style.background = 'green'
-    banner.style.color = 'yellow'
-    banner.style.display = 'flex'
-    banner.style['align-items'] = 'center'
-    banner.style['justify-content'] = 'center'
+    console.log('Begin inserting banner');
+    const banner = document.createElement('div');
+    banner.id = 'browixir-banner';
+    banner.style.width = '100vw';
+    banner.style.height = '8vw';
+    banner.style.position = 'absolute';
+    banner.style.top = 0;
+    banner.style.left = 0;
+    banner.style.background = 'green';
+    banner.style.color = 'yellow';
+    banner.style.display = 'flex';
+    banner.style['align-items'] = 'center';
+    banner.style['justify-content'] = 'center';
 
-    const copyContainer = document.createElement('span')
-    copyContainer.innerText = ${m}
-    const confirmButton = document.createElement('button')
-    confirmButton.innerText = 'Yes!'
-    confirmButton.addEventListener('click', () => chrome.runtime.sendMessage({ type: 'USER_CONFIRM', uuid: ${bid} }))
-    const rejectButton = document.createElement('button')
-    rejectButton.innerText = 'Nope'
-    rejectButton.addEventListener('click', () => chrome.runtime.sendMessage({ type: 'USER_REJECT', uuid: ${bid} }))
-    banner.appendChild(copyContainer)
-    banner.appendChild(confirmButton)
-    banner.appendChild(rejectButton)
-    console.log('Attaching banner to document')
-    document.body.appendChild(banner)
-    console.log('Attached banner to DOM')
-    document.addEventListener('visibilitychange', (_e) => {
-        if (document.hidden) {
-          console.log('Removing banner from DOM')
-          document.body.removeChild(banner)
-        }
-    });
-  `
+    const copyContainer = document.createElement('span');
+    copyContainer.innerText = ${m};
+    const confirmButton = document.createElement('button');
+    confirmButton.innerText = 'Yes!';
+    confirmButton.addEventListener('click', () => chrome.runtime.sendMessage({ type: 'USER_CONFIRM', uuid: ${bid} }));
+    const rejectButton = document.createElement('button');
+    rejectButton.innerText = 'Nope';
+    rejectButton.addEventListener('click', () => chrome.runtime.sendMessage({ type: 'USER_REJECT', uuid: ${bid} }));
+    banner.appendChild(copyContainer);
+    banner.appendChild(confirmButton);
+    banner.appendChild(rejectButton);
+    console.log('Attaching banner to document');
+    document.body.appendChild(banner);
+    console.log('Attached banner to DOM');`
+      // document.addEventListener('visibilitychange', (_e) => {
+      //     if (document.hidden) {
+      //       console.log('Removing banner from DOM')
+      //       document.body.removeChild(banner)
+      //     }
+      // });
     })
     .then((resultOnEveryPage: any[]) => {
       // The below is entirely usesless, the real way relies on oneShotEventHandler, but I wanted to test out returns
@@ -64,16 +63,20 @@ export const addBannerToActiveTab = (
   activityName = "Unknown Task"
 ): Promise<boolean> => {
   const addBannerToTab = addSelfCleaningBannerToTab(activityName);
-  getActiveTab().then((tab: Tabs.Tab) => addBannerToTab(tab.id));
-  return new Promise(resolve =>
-    browser.tabs.onActivated.addListener(({ tabId }) =>
-      addBannerToTab(tabId)
-        .then((...eventArgs) => {
-          const message: any = eventArgs[0];
-          if (message.type === "USER_CONFIRM") return true;
-          return false;
-        })
-        .then((res: boolean) => resolve(res))
-    )
-  );
+  return getActiveTab()
+    .then((tab: Tabs.Tab) => addBannerToTab(tab.id))
+    .then(
+      () =>
+        new Promise(resolve =>
+          browser.tabs.onActivated.addListener(({ tabId }) =>
+            addBannerToTab(tabId)
+              .then((...eventArgs) => {
+                const message: any = eventArgs[0];
+                if (message.type === "USER_CONFIRM") return true;
+                return false;
+              })
+              .then((res: boolean) => resolve(res))
+          )
+        )
+    );
 };
