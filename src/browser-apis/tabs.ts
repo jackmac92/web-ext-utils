534ffaf9c43acbd8aba38e9c4a6752f9898aa430
Jack McCown <jmccown@cbinsights.com>
2020-04-09 22:58:32 -0400

    feat: update reloadTab func to wait for reloaded

--- src/browser-apis/tabs.ts
+++ src/browser-apis/tabs.ts
@@ -1,4 +1,5 @@
-import { browser, Tabs } from 'webextension-polyfill-ts' // eslint-disable-line no-unused-vars
+import { browser, Tabs } from "webextension-polyfill-ts"; // eslint-disable-line no-unused-vars
+import { oneShotEventHandler } from "../helpers";
 
 export const getActiveTab = (): Promise<any> =>
   browser.windows
@@ -6,18 +7,28 @@ export const getActiveTab = (): Promise<any> =>
     .then(({ id }) => browser.tabs.query({ active: true, windowId: id }))
     .then(tabs => {
       if (!tabs[0]) {
-        throw new Error('Could not find an active tab')
+        throw new Error("Could not find an active tab");
       }
-      return tabs[0]
-    })
+      return tabs[0];
+    });
 
-const getALlTabs = () => browser.tabs.query({})
+const getALlTabs = () => browser.tabs.query({});
 
 export const withEachTab = async <T>(
   cb: (a: Tabs.Tab) => Promise<T>
 ): Promise<T[]> =>
   getALlTabs()
     .then(tabs => tabs.map(t => cb(t)))
-    .then(promises => Promise.all(promises))
+    .then(promises => Promise.all(promises));
 
-export const reloadTab = (tabId: number) => browser.tabs.reload(tabId)
+export const reloadTab = async (tabId: number): Promise<unknown> =>
+  browser.tabs.reload(tabId).then(() =>
+    oneShotEventHandler(
+      browser.tabs.onUpdated,
+      async ([eventTabId, { status }, _tab]) => {
+        if (tabId !== eventTabId) return false;
+        if (status === "complete") return true;
+        return false;
+      }
+    )
+  );
