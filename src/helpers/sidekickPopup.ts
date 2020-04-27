6a7f602463df1000780d5a218e27879b8873ee60
Jack McCown <jmccown@cbinsights.com>
2020-04-26 20:34:10 -0400

    feat: add window sidekick helper func

---
+++ src/helpers/sidekickPopup.ts
@@ -0,0 +1,27 @@
+import { browser, Windows } from "webextension-polyfill-ts";
+
+export default async function(url: string | string[]): Promise<Windows.Window> {
+  const window: Windows.Window = await browser.windows.getCurrent();
+  const { height, width, top, left } = window;
+  if (
+    !(
+      typeof height === "number" &&
+      typeof width === "number" &&
+      typeof left === "number" &&
+      typeof top === "number"
+    )
+  ) {
+    throw Error("Failed to get the dimensions of the current window");
+  }
+  const sidekickWidth = Math.ceil(width / 5);
+  window.width = width - sidekickWidth;
+  window.left = left + sidekickWidth;
+  return browser.windows.create({
+    url,
+    type: "popup",
+    top,
+    left,
+    height,
+    width: sidekickWidth
+  });
+}
