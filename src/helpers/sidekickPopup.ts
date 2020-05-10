518d11297154052354f46731dd3dff2005a35067
Jack McCown <jmccown@cbinsights.com>
2020-05-10 00:20:24 -0400

    fix: adjust width of primary window

--- src/helpers/sidekickPopup.ts
+++ src/helpers/sidekickPopup.ts
@@ -4,11 +4,12 @@ export const createSidekickWindow = async (
   url: string | string[]
 ): Promise<Windows.Window> => {
   const window: Windows.Window = await browser.windows.getCurrent();
-  const { height, width, top, left } = window;
+  const { id, height, width, top, left } = window;
   if (
     !(
       typeof height === "number" &&
       typeof width === "number" &&
+      typeof id === "number" &&
       typeof left === "number" &&
       typeof top === "number"
     )
@@ -16,8 +17,10 @@ export const createSidekickWindow = async (
     throw Error("Failed to get the dimensions of the current window");
   }
   const sidekickWidth = Math.ceil(width / 5);
-  window.width = width - sidekickWidth;
-  window.left = left + sidekickWidth;
+  await browser.windows.update(id, {
+    width: width - sidekickWidth,
+    left: left + sidekickWidth
+  });
   return browser.windows.create({
     url,
     type: "popup",
