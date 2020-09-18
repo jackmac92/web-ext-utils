88e112479b8159d32774b2c3c7fd17bf6b389b50
Jack McCown <jmccown@cbinsights.com>
2020-09-18 18:20:15 -0400

    fix: copy to clipbaord helper

--- src/browser-apis/clipboard.ts
+++ src/browser-apis/clipboard.ts
@@ -1,6 +1,8 @@
 import { textNotification } from "./notifications";
+import { browser } from "webextension-polyfill-ts";
 
-export function writeToClipboard(newClip: string): Promise<void> {
+export function writeToClipboardViaNavigator(newClip: string): Promise<void> {
+  // NB: not using this one since it just don't work for extensions :noidea:, getting an error that the dom is not focused
   return (
     navigator.permissions
       // @ts-ignore
@@ -9,29 +11,34 @@ export function writeToClipboard(newClip: string): Promise<void> {
         if (result.state == "granted" || result.state == "prompt") {
           return navigator.clipboard
             .writeText(newClip)
-            .catch(err => Promise.reject(err));
+            .catch((err) => Promise.reject(err));
         }
         return Promise.reject("No clipboard access");
       })
   );
 }
+async function writeToClipboard(newClip: string): Promise<void> {
+  const bg = browser.extension.getBackgroundPage(); // get the background page
+  bg.document.body.innerHTML = ""; // clear the background page
 
-export function writeToClipboardViaContentScript(
-  newClip: string
-): Promise<boolean | void> {
-  return (
-    navigator.permissions
-      // @ts-ignore
-      .query({ name: "clipboard-write" })
-      .then((result: { state: string }) => {
-        if (result.state == "granted" || result.state == "prompt") {
-          return navigator.clipboard
-            .writeText(newClip)
-            .catch(err => Promise.reject(err));
-        }
-        return Promise.reject("No clipboard access");
-      })
-  );
+  // add a DIV, contentEditable=true, to accept the paste action
+  const helperElement = bg.document.createElement("textarea");
+  document.body.appendChild(helperElement);
+  helperElement.value = newClip;
+
+  // focus the helper div's content
+  var range = document.createRange();
+  range.selectNode(helperElement);
+  const currSelection = window.getSelection();
+  if (currSelection === null) {
+    throw Error("Couldn't find the current window selection");
+  }
+  currSelection.removeAllRanges();
+  currSelection.addRange(range);
+  helperElement.focus();
+
+  // trigger the paste action
+  bg.document.execCommand("copy");
 }
 
 export function promptToCopyViaNotification(
